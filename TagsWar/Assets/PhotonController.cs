cabeee53b4e730f16c8bbdeb77d6b68a13d2945a
KazumsaHayashi <kazumasaice1215@icloud.com>
2018-12-09 22:54:21 +0900

    ルームの繋ぎ

---
+++ TagsWar/Assets/PhotonController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonController : Photon.MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings(null);
+	}
+	
+	void OnConnectedToServer()
+	{
+		
+		Debug.Log("Enter");
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
